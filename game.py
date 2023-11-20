41a53be93a31a5b98393b82caf61cb6b758a34e6
Olateju <3legged01@gmail.com>
2023-11-20 19:25:09 +0000

    game.py modified in new branch ifstat

--- game.py
+++ game.py
@@ -1,9 +1,12 @@
 fname=input("Enter your name ")
 lenname=len(fname)
-fchar=fname[0]
-lchar=fname[-1]
-print(f"Hello {fname}! Welcome to namegame3")
-print(f"The length of your  name is {lenname}")
-print(f"The first character of your name is {fchar}")
-print(f"The last character of your name is {lchar}")
+if lenname >= 2:
+  print(f"Hello {fname}! Welcome to namegame3")  
+  fchar=fname[0]
+  lchar=fname[-1]
+  print(f"The length of your  name is {lenname}")
+  print(f"The first character of your name is {fchar}")
+  print(f"The last character of your name is {lchar}")
+else: 
+    print(f"Your name cannot be less than two characters long, try again!")
 
