3249714a8fac023b495f5ad3fc9a3f1ce1da447d
Olateju <3legged01@gmail.com>
2023-11-20 18:53:11 +0000

    game.py added

---
+++ game.py
@@ -0,0 +1,9 @@
+fname=input("Enter your name ")
+lenname=len(fname)
+fchar=fname[0]
+lchar=fname[-1]
+print(f"Hello {fname}! Welcome to namegame3")
+print(f"The length of your  name is {lenname}")
+print(f"The first character of your name is {fchar}")
+print(f"The last character of your name is {lchar}")
+
